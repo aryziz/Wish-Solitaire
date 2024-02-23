8abe68c029ca3f91cfe029b51c87ae8b37fb4e20
Aryan Aziz Mathiesen <aryanam@uia.no>
2024-02-23 16:10:19 +0100

    Game logic fix & game rules

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Wish Solitaire, a fun CLI card game!
 ## The project
 
 * A command-line single-player card game
-* Rules of the game [here](https://bicyclecards.com/how-to-play/wish-solitaire/)
+* Rules of the game [here](https://semicolon.com/Solitaire/Rules/TheWish.html)
 * Project I did after taking learning python basics after taking INFO102 at UiB (Universitetet i Bergen)
 
 ## What I learned
--- wish-solitaire.py
+++ wish-solitaire.py
@@ -1,5 +1,5 @@
 from time import sleep
-from os import getcwd
+from os import getcwd, system
 from os.path import isfile
 from string import ascii_uppercase
 from random import shuffle
@@ -55,7 +55,8 @@ def remove_pair(card_1, card_2, card):
             deck[converter[card_2]].pop(0)
             deck[converter[card_1]].pop(0)
         else:
-            print("Error. Please try again")
+            print("Not valid. Try again.")
+            sleep(2)
     except IndexError:
         print("You have chosen an empty deck, please try again.")
         start(card)
@@ -99,7 +100,8 @@ def save(card):
     else:
         print("Oops! Something went wrong, try again.")
         sleep(2)
-        save(card)
+
+    main()
 
 
 def start(card):
@@ -109,7 +111,8 @@ def start(card):
             input("<Enter> main menu")
             break
         elif loss(card):
-            print("You lost.")
+            game_layout(card)
+            print("\nYou lost.")
             input("<Enter> main menu")
             break
         else:
@@ -118,13 +121,12 @@ def start(card):
             except Exception:
                 print("Oops! Something went wrong, try again.")
                 break
-            print("\n<Save> to save the game")
-            print("<Main menu> to enter the main menu")
-            user = input("Pick two cards: ").upper().strip()
-            if user == "SAVE":
+            print("\n<S> to save the game")
+            print("<Q> to enter the main menu")
+            user = input("> ").upper().replace(" ", "")
+            if user == "S":
                 save(card)
-            elif user == "MAIN MENU":
-
+            elif user == "Q":
                 main()
             else:
                 if len(user) == 2:
@@ -136,6 +138,7 @@ def start(card):
 
 
 def game_layout(card):
+    system("cls")
     for index, item in enumerate(card):
         if len(item) > 0:
             print("{:>2}".format(letters[index]), end="\t")
@@ -167,6 +170,30 @@ def load():
         sleep(2)
 
 
+def game_rules():
+    system("cls")
+    while True:
+        print(
+            f"""
+                {'-'*50}
+                Wish Solitaire
+                {'-'*50}
+                
+                ** Layout **
+                Six piles of cards next to each other categorized by letters
+                
+                ** How to Play **
+                Select the stacks that has the same card value
+                To select piles, select two piles by entering the letter which belongs to the pile
+                ** Goal **
+                Discard all 32 cards
+            """
+        )
+        user_input = input("Press Q to exit\n> ").strip()
+        if user_input.upper() == "Q":
+            break
+
+
 def main():
 
     global deck
@@ -175,20 +202,20 @@ def main():
     ---------------
     1 - New Game
     2 - Load
-    3 - Game rules (website)
+    3 - Game rules
     9 - Exit
     ---------------
     """
 
     print(menu)
-    user_input = input("Choose action: ")
+    user_input = input("> ")
     if user_input == "1":
         deck = hand_out(pile_of_cards)
         start(deck)
     elif user_input == "2":
         load()
     elif user_input == "3":
-        webbrowser.open("https://semicolon.com/Solitaire/Rules/TheWish.html")
+        game_rules()
     elif user_input == "9":
         for i in range(0, 4):
             b = "Terminating" + "." * i
